1bf6bf11f358b1e3463f0bdfbc5608c5b822d67c
SalazarMalagon <andressalazar062858@gmail.com>
2025-02-11 10:41:32 -0500

    Actualizando proyecto del profe

---
+++ Class_work/Class 25-01-2025/Project 2/semantic.py
@@ -0,0 +1,23 @@
+"""This module represents the behavior of a semantic analyzer.
+
+Author: Carlos Andres Sierra <cavirguezs@udistrital.edu.co>
+"""
+
+
+# pylint: disable=too-few-public-methods
+class SemanticAnalyzer:
+    """This class represents the behavior of a semantic analyzer."""
+
+    def __init__(self, tokens_input: list):
+        self.tokens = tokens_input
+        self.notes = []
+
+    def analyze(self):
+        """This method analyzes the tokens and returns the notes."""
+        for token in self.tokens:
+            if token.type_ == "NOTE":
+                self.notes.append((token.value, None))
+            elif token.type_ == "DURATION":
+                if self.notes:
+                    self.notes[-1] = (self.notes[-1][0], token.value)
+        return self.notes
